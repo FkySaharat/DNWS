140294bbd26869d7dd73e354700ed85d4a5761fe
FkySaharat <saharat_d@cmu.ac.th>
2019-05-14 19:43:37 +0700

    create following/unfollowing
    create following/unfollowing

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -13,5 +13,20 @@ angular.module('followingList', ['ngRoute'])
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
       });
+
+      self.sendfollowing = function sendfollowing(username)
+      {
+        const data = "followingname=" + encodeURIComponent(username);
+        $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions).then(function (response) {
+            
+        });
+      }
+      self.unfollowing = function unfollowing(username) {
+            const data = "followingname=" + encodeURIComponent(username);
+            $http.delete('http://localhost:8080/twitterapi/following/?'+ data, requestOptions).then(function (response) {
+
+            });
+      }
+
     }]
 });
